7b9f61d19dbb0472c7911700d9df90a251218c8d
JOHNS507 <35981062+JOHNS507@users.noreply.github.com>
2018-03-22 13:13:25 -0500

    Add files via upload

---
+++ Homework5/HW#5-BrandonJohnson/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  HW#5-BrandonJohnson
+//
+//  Created by Johnson, Brandon   (johns507) on 3/20/18.
+//  Copyright © 2018 Johnson, Brandon   (johns507). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var Superview: UIView!
+    @IBOutlet weak var Root: UIStackView!
+    
+    override func viewDidLoad() {
+         
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+   
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Homework5/HW#5-BrandonJohnsonTests/HW_5_BrandonJohnsonTests.swift
@@ -0,0 +1,36 @@
+//
+//  HW_5_BrandonJohnsonTests.swift
+//  HW#5-BrandonJohnsonTests
+//
+//  Created by Johnson, Brandon   (johns507) on 3/20/18.
+//  Copyright © 2018 Johnson, Brandon   (johns507). All rights reserved.
+//
+
+import XCTest
+@testable import HW_5_BrandonJohnson
+
+class HW_5_BrandonJohnsonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
